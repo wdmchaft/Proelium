1d959f2bade72df1f21a50a51504b2aab853340d
Cameron Deans <cdeansy@gmail.com>
2011-12-17 19:31:53 +0000

    Initial Commit

---
+++ Proelium/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Proelium
+//
+//  Created by Cameron Deans on 17/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
